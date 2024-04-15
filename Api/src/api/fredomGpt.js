071aea765cd1939dea4bc05f80df19d55d3b72e3
wavymio <159768195+wavymio@users.noreply.github.com>
2024-04-15 12:52:44 +0100

    updated fredomGpt api

--- Api/src/api/fredomGpt.js
+++ Api/src/api/fredomGpt.js
@@ -1,1 +1,49 @@
+const axios = require('axios')
+const apiUrl = process.env.FREEDOMGPT_API_URL
+const freedomGptApiKey = process.env.FREEDOMGPT_API_KEY
+const getRandomMisunderstanding = require("../functions/randomMisunderstanding")
+const { sendTypingAction } = require("./sendMessage")
 
+const processFreeMessages = async (userText, chatId) => {
+    try {
+        if (!userText) {
+            console.error('Empty or undefined message text received.')
+            await sendTypingAction(chatId)
+            const responseText = getRandomMisunderstanding()
+            return responseText
+        }
+      
+        const response = await axios.post(apiUrl, {
+            "apiKey": freedomGptApiKey,
+            "messages": [
+              {
+                "role": "user",
+                "content": userText
+              }
+            ],
+            "temperature": 0.7,
+            "top_k": 40,
+            "top_p": 0.8,
+            "repetition_penalty": 1.1,
+            "batch_size": 4,
+            "max_new_tokens": 512
+        })
+
+        if (!response.data) {
+            const responseText = "I feel wierd, please give me a moment"
+        }
+  
+        const responseText = response.data.output.content
+        console.log(response.data.credits)
+
+        return responseText
+      
+    } catch (err) {
+        await sendTypingAction(chatId);
+        console.error("An unexpected error occurred:", err)
+        const responseText = `Yo some error occurred\nWait a while and then try again\nContact this guy if the issue persists @wavymio`
+        return responseText
+    }
+}
+
+module.export = processFreeMessages
